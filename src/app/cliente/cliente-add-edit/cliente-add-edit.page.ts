b93e55c2e060e3831d3cee7eb3b1447fbbaca756
fredkendy <fredkendy9@gmail.com>
2024-01-10 14:08:03 -0300

    recuperacao de dados

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteAddEditPage implements OnInit {
 
+  //Exemplo de binding. Decorator de propriedades, que busca no DOM da pág pelo componente enviado como primeiro parametro
+  //entrega uma referencia na propriedade nome (nome poderia ser qqr elemento HTML)
+  @ViewChild('inputNome',	{read:	ElementRef})	nome:	ElementRef | undefined ;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //@ts-ignore
+  submit() {
+    console.log(this.nome?.nativeElement.value);
   }
 
 }
