2093bad2579e54a2864e398a5f642377b9cc8494
fredkendy <fredkendy9@gmail.com>
2024-01-10 18:03:40 -0300

    mensagem de confirmacao

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,13 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-interface Cliente {
-  nome: string;
-  email: string;
-  telefone: string;
-  renda: string;
-  nascimento: string;
-}
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -36,7 +29,7 @@ export class ClienteAddEditPage implements OnInit {
   clienteForm!: FormGroup;
 
   //visível na classe. Injeção de dependência do FormBuilder (permite usar recursos como FormGroup)
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private alertCtrl: AlertController, private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.clienteForm = this.formBuilder.group({
@@ -48,7 +41,7 @@ export class ClienteAddEditPage implements OnInit {
     });
   }
 
-  submit() {
+  async submit() {
     this.errorsMessage = [];
     if (this.clienteForm.get('nome')?.hasError('required')) {
       this.errorsMessage.push('Nome é obrigatório');
@@ -58,6 +51,32 @@ export class ClienteAddEditPage implements OnInit {
     }
     //recebe valor booleano de acordo com a existência de erros na matriz
     this.hasErrors = this.errorsMessage.length > 0;
+
+    if(!this.hasErrors) {
+      //escolher UI entre notificação ou alerta
+      //await this.presentAlert('Sucesso', 'Gravação bem sucedida', 'Os dados do cliente foram gravados', ['Ok']);
+      await this.presentToast('Gravação bem sucedida', 3000, 'top')
+    }
+  }
+
+  //continuidade de execução da aplicação enquanto o método não é concluído (evita sensação de travamento)
+  async presentAlert(header: string, subHeader: string, message: string, buttons: string[]) {
+    const alert = await this.alertCtrl.create({
+      header,
+      subHeader,
+      message,
+      buttons
+    });
+    await alert.present();
+  }
+
+  async presentToast(message: string, duration: number, position: 'top' | 'bottom') { //ou top ou bottom
+    const toast = await this.toastCtrl.create({
+      message,
+      duration,
+      position
+    });
+    toast.present();
   }
 
 }
